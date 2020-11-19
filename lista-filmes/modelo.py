aa94ff858161b84da969eed6c7416fafc5c8d71f
Caio Carvalho <caiocarvalho.py@gmail.com>
2020-11-19 17:31:31 -0400

    Herança

--- lista-filmes/modelo.py
+++ lista-filmes/modelo.py
@@ -41,14 +41,34 @@ class Serie(Programa):
         return f'{self.nome} - {self.ano} - {self.temporadas} temporadas - {self.likes} Likes'
 
 
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        super().__init__(programas)
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
-
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana:
     print(programa)
+
+print(f'Está na playlist? {demolidor in playlist_fim_de_semana}')
